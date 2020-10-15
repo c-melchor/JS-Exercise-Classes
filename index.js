7e6607f77c5f7cbc89a74b8f85f5a0325589350a
c_melchor <cah.melchor@gmail.com>
2020-10-15 08:45:10 -0700

    task 1 done

--- index.js
+++ index.js
@@ -41,8 +41,26 @@ class Airplane {
 */
 
 class Person {
-
+  constructor(name, age){
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
+  eat(edible){
+    if(this.stomach.length<=9){
+    this.stomach.push(edible);
+    }
+  }
+  poop(){
+    this.stomach =[];
+  }
+  toString(){
+    return `${this.name},${this.age}`;
+  }
 }
+
+
+
 
 /*
   TASK 2
@@ -59,7 +77,23 @@ class Person {
 */
 
 class Car {
-
+  constructor(model, milesPerGallon){
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  fill(gallons){
+    this.tank += gallons;
+  }
+  // drive(distance){
+  //   this.tank 
+  //   if(this.tank > 0){
+  //     distance += this.odometer;
+  //   } else {
+  //     return `I ran out of fuel at ${this.odometer} miles!`;
+  //   }
+  // }
 }
 
 /*
